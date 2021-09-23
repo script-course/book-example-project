f0f55ff7e93052e02c16af8666bc1317a9d4526f
Jasper Boerhof <jasper@script.nl>
2021-09-23 15:01:47 +0200

    typo..

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -14,7 +14,7 @@ export default {
     components: {Navbar},
     data() {
         return {
-            message: 'Gaan we dit nu in het nederlands of engels doen? Gaat nu wat door mekaar',
+            message: 'Gaan we dit nu in het nederlands of engels doen? Gaat nu wat door ELKAAR',
         };
     },
     mounted() {
